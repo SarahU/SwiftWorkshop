bf9e835fd33c6c203bfd8348b799bf119ba4677b
Sarah Usher <sarahu@zpg-c02v62juhtdd.local>
2018-07-24 20:27:25 +0100

    Initial Commit

---
+++ Lady2Lady/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lady2Lady
+//
+//  Created by Sarah Usher on 24/07/2018.
+//  Copyright © 2018 Sarah Usher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Lady2LadyTests/Lady2LadyTests.swift
@@ -0,0 +1,36 @@
+//
+//  Lady2LadyTests.swift
+//  Lady2LadyTests
+//
+//  Created by Sarah Usher on 24/07/2018.
+//  Copyright © 2018 Sarah Usher. All rights reserved.
+//
+
+import XCTest
+@testable import Lady2Lady
+
+class Lady2LadyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
